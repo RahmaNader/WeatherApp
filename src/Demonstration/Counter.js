ad09b729c1642bf88e1d6091152ce0ef65320cf7
RahmaNader <rahmanaderm@gmail.com>
2023-12-17 01:45:24 +0200

    done with counter demonstrations

--- src/Demonstration/Counter.js
+++ src/Demonstration/Counter.js
@@ -1,8 +1,14 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    useEffect(() => {
+        console.log(`the count value is ${count}`)
+        return () => {
+            console.log('clean up')
+        }
+    },[count]) 
     return(
         <View style={styles.wrapper}>
             <Text style={styles.count}>{`count: ${count}`}</Text>
@@ -42,3 +48,33 @@ const styles = StyleSheet.create({
     },
 })
 export default Counter;
+
+/*
+useEffect(() => {
+        console.log(`the count value is ${count}`)
+    }) 
+    the code repeats every time the component rerenders, we can modify the code so that it runs only when the count value changes: 
+    useEffect(() => {
+        console.log(`the count value is ${count}`)
+    },[count])
+
+    what effects with clean up do? 
+    useEffect(() => {
+        console.log(`the count value is ${count}`)
+        return () => {
+            console.log('clean up')
+        }
+    },[count]) 
+    the clean up code will run when the component unmounts, what's clean up code? 
+    clean up code is code that runs when the component unmounts.
+    
+    why do we clean up? 
+    we clean up to prevent memory leaks, memory leaks occur when we have unused data in memory, 
+    and this unused data can cause our app to slow down or even crash.
+
+    what are side effects?
+    side effects are anything that affect the outside world, 
+    for example, changing the state of a component is a side effect,
+    because it changes the outside world, and it changes the state of the component.
+
+*/
